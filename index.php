10a81ca142f8e5d69eb68e0e56eb3a82f473cb34
[Riccardo Uras] <[riccardo.uras3@gmail.com]>
2023-06-22 10:07:24 +0200

    un po' di css

--- index.php
+++ index.php
@@ -4,15 +4,43 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Animal Shop</title>
+    <style>
+        img{
+            width: 200px;
+        }
+        li{
+            list-style: none;
+            margin: 20px;
+        }
+        a{
+            text-decoration: none;
+        }
+        ul{
+            display: flex;
+        }
+    </style>
 </head>
 <body>
 <h1>Benvenuto nel  nostro Negozio di Animali!</h1>
     
     <h2>Categorie:</h2>
     <ul>
-        <li><a href="category.php?categoria=cibo">Cibo</a></li>
-        <li><a href="category.php?categoria=cuccia">Cuccia</a></li>
-        <li><a href="category.php?categoria=gioco">Gioco</a></li>
+        <li>
+            <img style="width: 200px;" src="https://th.bing.com/th/id/R.87cd5cef16d3267b94f87b78618f72ad?rik=2Mb2xT%2ft%2fVKoHg&pid=ImgRaw&r=0" alt="carlino">
+            <br> 
+            <a href="category.php?categoria=cibo">Cibo</a> 
+        </li>
+       
+        <li>
+            <img src="https://emporiogenova.com/2393-thickbox_default/cuccia-cani-gatti-esterno-legno.jpg" alt="">
+            <br> 
+            <a href="category.php?categoria=cuccia">Cuccia</a>
+        </li>
+        <li>
+            <img src="https://th.bing.com/th/id/OIP.Y1uAfzN13Y0CaueN-WIvPwHaEd?pid=ImgDet&rs=1" alt="">
+            <br> 
+            <a href="category.php?categoria=gioco">Gioco</a>
+        </li>
     </ul>
 </body>
 </html>
